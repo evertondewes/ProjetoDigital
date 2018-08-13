816349586829f4d71b3770cbcf90f9890f0d1519
Rafael <rafaelcatelandonascimento@gmail.com>
2018-08-12 22:04:32 -0300

    typo

--- resources/views/backend/projects/index.blade.php
+++ resources/views/backend/projects/index.blade.php
@@ -17,7 +17,7 @@
                         <th>Requerente(s)</th>
                         <th>Situação</th>
                         <th>Ação</th>
-                        <th>Analizar</th>
+                        <th>analisar</th>
                     </tr>
                 </thead>
 
@@ -37,7 +37,7 @@
                                     Adicionar evento
                                 </a>
                             </td>
-                            <td><a href="/project-docs-approve/{{$project->id}}">Analizar</a></td>
+                            <td><a href="/project-docs-approve/{{$project->id}}">analisar</a></td>
 
                         </tr>
                     @endforeach
